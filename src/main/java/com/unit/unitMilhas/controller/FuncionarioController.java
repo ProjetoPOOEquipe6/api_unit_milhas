e5969654579f355764c39fc8743685db72436663
Rafael Vieira <rafa.vieiradelima@gmail.com>
2022-06-11 12:00:19 -0300

    Conclusão funcionario e tipo função controller

--- src/main/java/com/unit/unitMilhas/controller/FuncionarioController.java
+++ src/main/java/com/unit/unitMilhas/controller/FuncionarioController.java
@@ -1,7 +1,15 @@
 package com.unit.unitMilhas.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.unit.unitMilhas.model.Funcionario;
+import com.unit.unitMilhas.repository.FuncionarioRepo;
+
 
 @RestController
 @RequestMapping(value="/employee") 
@@ -13,6 +21,23 @@ public class FuncionarioController {
     @GetMapping
     public List<Funcionario> findAll(){
         return employeeRepo.findAll();
+    }
+
+    @PostMapping
+    public Funcionario save(@RequestBody Funcionario novoFuncionario){
+        return employeeRepo.save(novoFuncionario);
+    }
+    
+
+    @PutMapping
+    public Funcionario update(@RequestBody Funcionario funcionarioAtualizar){
+        return employeeRepo.save(funcionarioAtualizar);
+    }
+
+    @DeleteMapping
+    public String delete(@RequestBody Funcionario funcionarioDeletar){
+        employeeRepo.delete(funcionarioDeletar);
+        return "Funcionario deletado com sucesso!";
     }
 
     
---
+++ src/main/java/com/unit/unitMilhas/controller/TipoFuncaoController.java
@@ -0,0 +1,43 @@
+package com.unit.unitMilhas.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ResquestMapping(value="/function")
+
+public class TipoFuncaoController {
+    @Autowired
+    private TipoFuncaoRepo functionRepo;
+
+    @GetMapping
+    public List<TipoFuncao> findAll(){
+        return functionRepo.findAll();
+    }
+
+    @PostMapping 
+    public TipoFuncao save(@RequestBody TipoFuncao novaTipoFuncao){
+        return functionRepo.save(novaTipoFuncao);
+    }
+
+    @PutMapping
+    public TipoFuncao update(@RequestBody TipoFuncao TipoFuncaoAtualizar){
+        return functionRepo.save(TipoFuncaoAtualizar);
+    }
+
+    @DeleteMapping
+    public String delete(@RequestBody TipoFuncao TipoFuncaoDeletar){
+        return "Tipo função deletada com sucesso!";
+    }
+
+
+    
+}
