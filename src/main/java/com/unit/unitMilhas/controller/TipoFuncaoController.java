70f7aeddb9947078148ce6936a7ed145f199cfbe
Thyago Santos <thiago_1991t@hotmail.com>
2022-06-11 12:06:48 -0300

    Ajuste no import (FuncionarioController e TipoFuncaoController)

--- src/main/java/com/unit/unitMilhas/controller/TipoFuncaoController.java
+++ src/main/java/com/unit/unitMilhas/controller/TipoFuncaoController.java
@@ -11,9 +11,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@ResquestMapping(value="/function")
+import com.unit.unitMilhas.model.TipoFuncao;
+import com.unit.unitMilhas.repository.TipoFuncaoRepo;
 
+@RestController
+@RequestMapping(value="/function")
 public class TipoFuncaoController {
     @Autowired
     private TipoFuncaoRepo functionRepo;
